526b853cfbabde703d348a0e92c8456885dbea0d
Juliy V. Chirkov <operator@juliyvchirkov.dev>
2024-07-02 20:47:11 +0300

    Extended edition of which function

---
+++ src/which.extended.sh
@@ -0,0 +1,139 @@
+##################################
+# shellcheck shell=sh enable=all #
+##################################
+
+which() (
+    if [ $# -gt 0 ]; then
+        while [ $# -gt 0 ]; do
+            case "${1}" in
+            --)
+                shift
+
+                break
+
+                ;;
+            -i)
+                searchonlypath=1
+
+                shift
+
+                ;;
+            -a)
+                locateall=1
+
+                shift
+
+                ;;
+            -s)
+                besilent=1
+
+                shift
+
+                ;;
+            -ia | -ai)
+                searchonlypath=1
+                locateall=1
+
+                shift
+
+                ;;
+            -is | -si)
+                searchonlypath=1
+                besilent=1
+
+                shift
+
+                ;;
+            -sa | -as)
+                locateall=1
+                besilent=1
+
+                shift
+
+                ;;
+            -ias | -isa | -ais | -asi | -sia | -sai)
+                searchonlypath=1
+                locateall=1
+                besilent=1
+
+                shift
+
+                ;;
+            -*)
+                printf -- "which: invalid option — %s\n\n" "${1#-}" >&2
+
+                exitstatus=2
+
+                set --
+
+                break
+
+                ;;
+            *)
+                break
+
+                ;;
+            esac
+        done
+    fi
+
+    if [ $# -gt 0 ]; then
+        for command; do
+            located=
+
+            [ -n "${searchonlypath:-}" ] || {
+                cmd="$(LC_ALL=C command -v -- "${command}" 2>/dev/null)"
+
+                if [ -z "${cmd}" ]; then
+                    exitstatus=1
+
+                    continue
+                elif [ "${cmd}" = "${command}" ] || [ ! -x "${cmd}" ]; then
+                    [ "${cmd}" = "${command}" ] || {
+                        cmd="${cmd#*\'}"
+                        cmd="${cmd%\'}"
+                    }
+
+                    located=1
+
+                    [ -n "${besilent:-}" ] || printf -- "%s\n" "${cmd}"
+
+                    [ -n "${locateall:-}" ] || continue
+                fi
+            }
+
+            IFS=":"
+
+            #############################
+            # shellcheck disable=SC2086 #
+            #############################
+            for path in ${PATH}; do
+                if [ -x "${path}/${command}" ]; then
+                    located=1
+
+                    [ -n "${besilent:-}" ] || printf -- "%s/%s\n" "${path}" "${command}"
+
+                    [ -n "${locateall:-}" ] || break
+                fi
+            done
+
+            [ -n "${located}" ] || exitstatus=1
+        done
+
+        return "${exitstatus:-0}"
+    fi
+
+    printf "%s\n\n" "which: locates command(s) and reports to standard output" >&2
+    printf "%s\n" "USAGE" >&2
+    printf "    %s\n" "which [-ias] command …" >&2
+    printf "%s\n" "OPTIONS" >&2
+    printf "    %s     %s\n" "-i" "ignore shell builtins, functions and aliases" >&2
+    printf "    %s     %s\n" "-a" "print all matches" >&2
+    printf "    %s     %s\n" "-s" "silently return 0 if all commands are located or 1 otherwise" >&2
+    printf "%s\n" "EXIT STATUS" >&2
+    printf "    %s      %s\n" 0 "all commands are located" >&2
+    printf "    %s      %s\n" 1 "failed to locate some command(s)" >&2
+    printf "    %s      %s\n" 2 "an invalid option is specified" >&2
+
+    return "${exitstatus:-1}"
+)
