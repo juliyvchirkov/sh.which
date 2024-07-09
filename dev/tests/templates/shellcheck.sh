b2e9183d756defb455339cd0bbf67cc5900fc2a3
Juliy V. Chirkov <operator@juliyvchirkov.dev>
2024-07-09 08:23:03 +0300

    Removes redundant symlink to .shellcheckrc config for Shellcheck linter and tunes the corresponding test template to force shellcheck utilize the config under dev folder

--- dev/tests/templates/shellcheck.sh
+++ dev/tests/templates/shellcheck.sh
@@ -2,12 +2,12 @@ Describe "shellcheck"
   Skip if "shellcheck command is not installed" [ ! -x bin/shellcheck ]
 
   Example "which.classic.sh"
-    When run bin/shellcheck --shell=sh --format=quiet ../src/which.classic.sh
+    When run bin/shellcheck --rcfile=.shellcheckrc --shell=sh --format=quiet ../src/which.classic.sh
     The status should equal 0
   End
 
   Example "which.extended.sh"
-    When run bin/shellcheck --shell=sh --format=quiet ../src/which.extended.sh
+    When run bin/shellcheck --rcfile=.shellcheckrc --shell=sh --format=quiet ../src/which.extended.sh
     The status should equal 0
   End
 End
